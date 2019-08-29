202da6233432af139244aca8dc3fb6459d186120
terrypls <selvastian@gmail.com>
2019-08-29 11:59:58 -0400

    esqueleto de funciones a implementar

--- src/Tarea1/Red_Neuronal/CapaNeurona.kt
+++ src/Tarea1/Red_Neuronal/CapaNeurona.kt
@@ -3,6 +3,9 @@ package Tarea1.Red_Neuronal
 import Tarea1.Red_Neuronal.Funciones_Activacion.FuncionesActivacion
 
 class CapaNeurona(val cantNeuronas:Int, val funcion:FuncionesActivacion) {
-
     //TODO definir los argumentos que tiene que recibir la capa de la neurona
+
+    fun entrenarCapa(){}
+    fun aprenderCapa(){}
+    fun backPropagationCapa(){}
 }
--- src/Tarea1/Red_Neuronal/RedNeuronal.kt
+++ src/Tarea1/Red_Neuronal/RedNeuronal.kt
@@ -1,5 +1,14 @@
 package Tarea1.Red_Neuronal
 
-class RedNeuronal (val capas:Int, val neuronasPorCapa:List<Int>,val entradas:Int,salidas:Int){
+class RedNeuronal(
+    val capas: Int,
+    val neuronasPorCapa: List<Int>,
+    val entradas: Int,
+    salidas: Int
+) {
+
+    fun entrenarRed(){}
+    fun aprenderRed(){}
+    fun backPropagation(){}
     //TODO crear capas de la neurona
 }
