b87f0919e1027d3fd030c1f459ed420e30e7ff8d
terrypls <selvastian@gmail.com>
2019-08-31 17:41:45 -0400

    red neuronal lista, sin test generados

--- src/Tarea1/Red_Neuronal/RedNeuronal.kt
+++ src/Tarea1/Red_Neuronal/RedNeuronal.kt
@@ -1,16 +1,131 @@
 package Tarea1.Red_Neuronal
 
-import Tarea1.Red_Neuronal.Funciones_Activacion.FuncionesActivacion
-import Tarea1.Red_Neuronal.Funciones_Activacion.Sigmoid
+import kotlin.random.Random
 
+/**
+ * @author Sebastian Donoso
+ */
+
+/**
+ * clase que se encarga de la creacion de una red neuronal
+ * @param numeroCapas cantidad de capas de la red neuronal
+ * @param neuronasPorCapa especifica la cantidad de neuronars presentes en cada una de las capas
+ * @param entradas cantidad de valores de entradas que soportara la red
+ * @param salidas cantidad de valores de salida que se espera que tenga la red
+ */
 class RedNeuronal(
-    private val capas: Int,
+    private val numeroCapas: Int,
     private val neuronasPorCapa: List<Int>,
     private val entradas: Int,
-    private val salidas: Int,
-    private val epoch: Int
-
+    private val salidas: Int
 ) {
+    private lateinit var valor:List<Double>
+    private lateinit var capas: Array<CapaNeurona?>
+    var precision: MutableList<Double> = mutableListOf()
+    //lista para almacenar los set de datos suministrados a la red
+    private val data: MutableList<DataSet> =
+        mutableListOf()
 
-    //TODO crear capas de la neurona
+    /**
+     * clase interna para guardar informacion sobre los set de datos que se le suministraran a la red para entrenamiento
+     * @param inputs valores entregados a la red
+     * @param outputs valores esperados de la red
+     */
+    internal data class DataSet(val inputs: List<Double>, val outputs: List<Double>)
+
+
+    /**
+     * metodo para agregar un nuevo set de datos/respuesta para entrenar la red
+     * @param inputs valores suministrados a la red
+     * @param outputs valores esperados de la red
+     */
+    fun agregarSet(inputs: List<Double>, outputs: List<Double>) {
+        data.add(DataSet(inputs, outputs))
+    }
+
+
+    /**
+     * metodo que crea la red neuronal con los parametros dados por defecto al momento de armar la red, por defecto es una red
+     * hecha a partir de neuronas de tipo sigmoids
+     */
+    fun crearRed() {
+        assert(numeroCapas > 0) { "la red debe tener al menos 1 capa" }
+        assert(neuronasPorCapa.last() == salidas) { "Cantidad de neuronas en la salidad no concuerda con el input dado" }
+        //crea el arreglo en donde se guardaran las capas
+        capas = arrayOfNulls(numeroCapas)
+        capas.withIndex().forEach { (i, capa) ->
+            capas[i] = CapaNeurona(
+                neuronasPorCapa[i],
+                capaPrevia = when {
+                    i - 1 < 0 -> null
+                    else -> capas[i - 1]
+                }
+            )
+            //le dice a cada capa quien es su siguiente
+            capa!!.capaSiguiente = when (i) {
+                numeroCapas -> null
+                else -> capas[i + 1]
+            }
+            //creamos las neuronas de cada una de las capas
+            capa.crearNeuronas(
+                pesos = when {
+                    capa.capaPrevia != null -> capas[i]!!.salidas.size
+                    else -> entradas
+                }
+            )
+
+        }
+    }
+
+    /**
+     * metodo para entrenar la red una cantidad definida de veces tomando un conjunto input/output definido en la clase
+     * dataSet
+     * @param repeticiones cantidad de veces que se entrenara
+     */
+    fun entrenarRed(repeticiones: Int) {
+        for (i in 0 until repeticiones) {
+            var acierto = 0.toDouble()
+            //toma un indice aleatorio para comparar y ver la precision
+            val indice = Random.nextInt(0,valor.size)
+            data.shuffle()//desordenamos la lista
+            data.forEach { (input, output) ->
+                entrenarRed(input, output)
+
+                if(valor[indice]==output[indice])acierto++
+            }
+            precision.add(acierto/(data.size*100))
+        }
+
+    }
+
+    fun entrenarRed(inputs: List<Double>, outputs: List<Double>) {
+        alimentarRed(inputs)
+        valor = capas.last()!!.salidas
+        backPropagationError(outputs)
+        actualizarPesos(inputs)
+    }
+
+    fun alimentarRed(inputs: List<Double>) {
+        capas.first()!!.entrenarCapa(inputs)
+    }
+
+    fun backPropagationError(esperado: List<Double>) {
+        capas.last()!!.backPropagationError(esperado)
+    }
+
+    fun actualizarPesos(inputs: List<Double>) {
+        capas.first()!!.actualizarPesos(inputs)
+    }
+
+    /**
+     * normaliza un valor al rango [0,1]
+     * @param valor valor a normalizar
+     * @param min   valor minimo que puede tomar el @param valor
+     * @param max valor maximo que puede tomar el @param valor
+     * @return valor normalizado
+     */
+    fun normalizar(valor: Double, min: Double, max: Double): Double {
+        return (valor - min) / (max - min)
+    }
+
 }
---
+++ test/Tarea1/Red_Neuronal/RedNeuronalTest.kt
@@ -0,0 +1,53 @@
+package Tarea1.Red_Neuronal
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class RedNeuronalTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @Test
+    fun getPrecision() {
+    }
+
+    @Test
+    fun setPrecision() {
+    }
+
+    @Test
+    fun agregarSet() {
+    }
+
+    @Test
+    fun crearRed() {
+    }
+
+    @Test
+    fun entrenarRed() {
+    }
+
+    @Test
+    fun testEntrenarRed() {
+    }
+
+    @Test
+    fun alimentarRed() {
+    }
+
+    @Test
+    fun backPropagationError() {
+    }
+
+    @Test
+    fun actualizarPesos() {
+    }
+
+    @Test
+    fun normalizar() {
+    }
+}
