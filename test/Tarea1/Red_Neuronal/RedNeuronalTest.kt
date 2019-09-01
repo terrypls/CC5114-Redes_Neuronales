b58062b9d8b4b249fad7194beae4c70cebcdbc6f
terrypls <selvastian@gmail.com>
2019-09-01 10:29:38 -0400

    test funcional de la red con un and

--- test/Tarea1/Red_Neuronal/RedNeuronalTest.kt
+++ test/Tarea1/Red_Neuronal/RedNeuronalTest.kt
@@ -6,48 +6,44 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 internal class RedNeuronalTest {
-
+    private lateinit var red: RedNeuronal
+    var l11 = listOf(1.0, 1.0)
+    var l10 = listOf(1.0, .0)
+    var l01 = listOf(.0, 1.0)
+    var l00 = listOf(.0, .0)
+    private var repeticiones = 100000
     @BeforeEach
     fun setUp() {
+        red = RedNeuronal(2, listOf(5, 2), 2, 2)
+    }
+
+
+    @Test
+    fun and() {
+        assertEquals(0, red.data.size)
+        red.agregarSet(l00, listOf(1.0,.0))
+        red.agregarSet(l01, listOf(1.0,.0))
+        red.agregarSet(l10, listOf(1.0,.0))
+        red.agregarSet(l11, listOf(.0,1.0))
+        red.entrenarRed(repeticiones)
+        println(red.precision)
+        assertEquals(1,red.elige(listOf(1.0,1.0)))
+        assertEquals(0,red.elige(listOf(1.0,0.0)))
+        assertEquals(0,red.elige(listOf(0.0,1.0)))
+        assertEquals(0,red.elige(listOf(0.0,0.0)))
+
+
+
+
     }
 
     @Test
-    fun getPrecision() {
+    fun or() {
     }
 
     @Test
-    fun setPrecision() {
+    fun nand() {
     }
 
-    @Test
-    fun agregarSet() {
-    }
-
-    @Test
-    fun crearRed() {
-    }
-
-    @Test
-    fun entrenarRed() {
-    }
-
-    @Test
-    fun testEntrenarRed() {
-    }
-
-    @Test
-    fun alimentarRed() {
-    }
-
-    @Test
-    fun backPropagationError() {
-    }
-
-    @Test
-    fun actualizarPesos() {
-    }
-
-    @Test
-    fun normalizar() {
-    }
+    fun xor() {}
 }
